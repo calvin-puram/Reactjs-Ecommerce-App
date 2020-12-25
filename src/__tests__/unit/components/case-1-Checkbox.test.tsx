6f862f1ddc244fd70bc3e462ebdc5fd560dde68e
calvin <puram.calvin@gmail.com>
2020-12-25 16:44:44 +0100

    setup unit test for custom checkbox

--- src/__tests__/unit/components/case-1-Checkbox.test.tsx
+++ src/__tests__/unit/components/case-1-Checkbox.test.tsx
@@ -2,16 +2,9 @@ import React from 'react'
 import { axe } from 'jest-axe'
 import { render, fireEvent } from '@testing-library/react'
 import ReactDom from 'react-dom'
-// import jest from 'jest'
 
 import Checkbox from '../../../components/Checkbox'
 
-/**
- * This checkbox component renders a checkbox with a label.
- * Since we customized the default checkbox, we want to
- * make sure it still works as a regular checkbox
- * should.
- */
 describe('The <Checkbox /> component', () => {
   const CheckboxProps = {
     label: 'checkbox checker',
@@ -21,20 +14,37 @@ describe('The <Checkbox /> component', () => {
     checkMarkBackground: '#fff',
     onChange: jest.fn(),
   }
-  const setupCheckBox = () => render(<Checkbox {...CheckboxProps} />)
+  const setupCheckBox = (props = CheckboxProps) =>
+    render(<Checkbox {...props} />)
   it(' Should render the label and checkbox the user will see', () => {
     const { asFragment } = setupCheckBox()
     expect(asFragment()).toMatchSnapshot()
   })
 
   it(' Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {
-    const { getByLabelText, debug } = setupCheckBox()
+    const { getByLabelText } = setupCheckBox()
     expect(getByLabelText(CheckboxProps.label)).toBeInTheDocument()
   })
 
-  it('❌ Should call the onChange handler when it is provided', () => {})
+  it('Should call the onChange handler when it is provided', () => {
+    const { getByLabelText } = setupCheckBox()
+    fireEvent.click(getByLabelText(CheckboxProps.label))
 
-  it('❌ Should change state correctly when clicked (checked and unchecked)', () => {})
+    expect(CheckboxProps.onChange).toHaveBeenCalled()
+  })
 
-  it('❌ should not fail any accessibility tests', async () => {})
+  it('Should change state correctly when clicked (checked and unchecked)', () => {
+    const { getByLabelText } = setupCheckBox({
+      ...CheckboxProps,
+      checked: true,
+    })
+
+    expect(getByLabelText(CheckboxProps.label)).toBeChecked()
+  })
+
+  it('should not fail any accessibility tests', async () => {
+    const { container } = setupCheckBox()
+
+    expect(await axe(container)).toHaveNoViolations()
+  })
 })
