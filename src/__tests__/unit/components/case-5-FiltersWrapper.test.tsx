0cfca3e75eb20fe7acd03a826c1c626c99902169
calvin <puram.calvin@gmail.com>
2020-12-26 09:54:34 +0100

    unit test the filter wrapper functionality

--- src/__tests__/unit/components/case-5-FiltersWrapper.test.tsx
+++ src/__tests__/unit/components/case-5-FiltersWrapper.test.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 
-import useFilters from '../../../hooks/useFilters'
 
 import { FiltersContext } from '../../../context/filters'
 import { FiltersWrapper } from '../../../components/FiltersWrapper'
 
 describe('The <FiltersWrapper /> component', () => {
-  it('❌ should render all children passed to it', () => {})
+  it('should render all children passed to it', () => {
+    const {getByTestId} = render(
+    <FiltersWrapper>
+     <div data-testid="filtertest">testing children</div>
+    </FiltersWrapper>)
+    
+    expect(getByTestId('filtertest')).toBeInTheDocument()
+  })
 
-  it('❌ should update the filters context with correct state values', () => {})
+  it(' should update the filters context with correct state values', () => {
+    const hide_filter = 'Hide Filter'
+    const show_filter = 'Show Filter'
+    const {getByText} = render(
+    <FiltersWrapper>
+    <FiltersContext.Consumer>
+     {({toggleShowingFilters, showingFilters} ) => {
+       return (
+         <button onClick={toggleShowingFilters}>{showingFilters ? show_filter: hide_filter}</button>
+       )
+     }}
+    </FiltersContext.Consumer>
+    </FiltersWrapper>)
 
-  it('❌ should update the body style to prevent scrolling when filter is toggled', () => {})
+    expect(getByText(hide_filter)).toBeInTheDocument();
+    fireEvent.click(getByText(hide_filter))
+    expect(getByText(show_filter)).toBeInTheDocument();
+  })
+
+  
 })
