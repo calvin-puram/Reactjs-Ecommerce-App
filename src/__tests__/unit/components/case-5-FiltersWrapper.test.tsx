23181c67860ee2fc7611fae186cd55954786aaf9
calvin <puram.calvin@gmail.com>
2020-12-27 09:43:56 +0100

    unit test the filter wrapper and usefilter hook

--- src/__tests__/unit/components/case-5-FiltersWrapper.test.tsx
+++ src/__tests__/unit/components/case-5-FiltersWrapper.test.tsx
@@ -6,6 +6,18 @@ import { FiltersContext } from '../../../context/filters'
 import { FiltersWrapper } from '../../../components/FiltersWrapper'
 
 describe('The <FiltersWrapper /> component', () => {
+  const hide_filter = 'Hide Filter'
+  const show_filter = 'Show Filter'
+  const setupFilters = () => render(
+    <FiltersWrapper>
+    <FiltersContext.Consumer>
+     {({toggleShowingFilters, showingFilters} ) => {
+       return (
+         <button onClick={toggleShowingFilters}>{showingFilters ? show_filter: hide_filter}</button>
+       )
+     }}
+    </FiltersContext.Consumer>
+    </FiltersWrapper>)
   it('should render all children passed to it', () => {
     const {getByTestId} = render(
     <FiltersWrapper>
@@ -16,23 +28,21 @@ describe('The <FiltersWrapper /> component', () => {
   })
 
   it(' should update the filters context with correct state values', () => {
-    const hide_filter = 'Hide Filter'
-    const show_filter = 'Show Filter'
-    const {getByText} = render(
-    <FiltersWrapper>
-    <FiltersContext.Consumer>
-     {({toggleShowingFilters, showingFilters} ) => {
-       return (
-         <button onClick={toggleShowingFilters}>{showingFilters ? show_filter: hide_filter}</button>
-       )
-     }}
-    </FiltersContext.Consumer>
-    </FiltersWrapper>)
+    const {getByText} = setupFilters()
 
     expect(getByText(hide_filter)).toBeInTheDocument();
     fireEvent.click(getByText(hide_filter))
     expect(getByText(show_filter)).toBeInTheDocument();
   })
 
+ it(' should update the body style to prevent scrolling when filter is toggled', () => {
+    const {getByText, debug} = setupFilters()
+
+    fireEvent.click(getByText(hide_filter))
+    expect(document.body.style.overflow).toBe('hidden');
+
+    fireEvent.click(getByText(show_filter))
+    expect(document.body.style.overflow).toBe('scroll');
+ })
   
 })
