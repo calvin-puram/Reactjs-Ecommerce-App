07064460c3938b43fc5660a64cd480bfe4ee7c7a
calvin <puram.calvin@gmail.com>
2020-12-27 16:25:20 +0100

    completed the app test

--- src/__tests__/integration/app.test.tsx
+++ src/__tests__/integration/app.test.tsx
@@ -8,10 +8,60 @@ import App from '../../components/App'
 import { createStore } from '../../store'
 import { FiltersWrapper } from '../../components/FiltersWrapper'
 
+jest.mock('../../helpers/axios')
+const mockAxios = Axios as any
+
 describe('The app ', () => {
-  test('❌ it fetches and renders all products on the page', async () => {})
+  const setupApp = () => render(
+      <StoreProvider store={createStore() as any}>
+        <FiltersWrapper>
+          <App />
+        </FiltersWrapper>
+      </StoreProvider>
+    )
+  test(' it fetches and renders all products on the page', async () => {
+    mockAxios.get.mockResolvedValue({
+      data: [
+        {
+          id: 1232233,
+          image: 'https://calvin',
+          name: 'new addidas',
+          price: 'from $230',
+          isSoldOut: false,
+          isActive: true,
+          createdAt: '2020-07-02T13:59:07+02:00',
+          brand: 'Marke',
+          isNew: true,
+          priceUnformatted: 2299,
+        },
+        {
+          id: 12322398,
+          image: 'https://calvin',
+          name: 'new addidas',
+          price: 'from $230',
+          isSoldOut: false,
+          isActive: true,
+          createdAt: '2020-07-02T13:59:07+02:00',
+          brand: 'Marke',
+          isNew: true,
+          priceUnformatted: 2299,
+        },
+      ],
+    })
 
-  test('❌ it can open and close the filters panel', async () => {})
+    const { findAllByTestId } = setupApp()
+    expect(await findAllByTestId('ProductTile')).toHaveLength(2);
+  })
 
-  test('❌ it can search products as user types in the search field', async () => {})
+  test(' it can open and close the filters panel', async () => {
+     const { getByText, queryByText } = setupApp()
+
+     expect(queryByText(/Reset to defaults/i)).not.toBeInTheDocument();
+     fireEvent.click(getByText(/filter/i))
+     expect(queryByText(/Reset to defaults/i)).toBeInTheDocument();
+     fireEvent.click(getByText(/view Results/i))
+     expect(queryByText(/Reset to defaults/i)).not.toBeInTheDocument();
+  })
+
+  
 })
--- src/__tests__/unit/hooks/case-7-useOutsideClick.test.tsx
+++ src/__tests__/unit/hooks/case-7-useOutsideClick.test.tsx
@@ -4,11 +4,51 @@ import { render, fireEvent } from '@testing-library/react'
 import useOutsideClick from '../../../hooks/useOutsideClick'
 
 describe('The useOutsideClick hook', () => {
-  // This test is important if you are going to be creating libraries in future.
-  // Very important to make sure the component is used the right way.
-  // This test can be migrated in future, if the useOutsideClick
-  // hook is moved to an external library at your company.
-  it('❌ calls the outside click handler when an outside click is initiated', () => {})
+ const TogglePanelButton = () => {
+   const [showing, setShowing] = useState(true);
 
-  it('❌ cleans up the event listeners after component is unmounted', () => {})
+   return(
+     <div >
+       <button onClick={() => setShowing(false)} data-testid="togglePanelButton">Panel Button</button>
+       {showing ? <Panel /> : null}
+     </div>
+   )
+ }
+
+  const Panel = () => {
+    const ref = useRef(null);
+    const [showing, setShowing] = useState(false);
+
+     useOutsideClick (ref, () => {
+       setShowing(!showing);
+     })
+
+     return(
+       <div ref={ref}>
+         <button data-testid="panelButton">
+           {showing ? 'SHOW BUTTON': 'HIDE BUTTON'}
+         </button>
+       </div>
+     )
+  }
+  it('calls the outside click handler when an outside click is initiated', () => {
+     const {getByTestId} = render(
+       <div>
+         <button data-testid="outsidePanelButton">outside</button>
+         <Panel />
+       </div>
+     )
+     
+     expect(getByTestId('panelButton')).toHaveTextContent('HIDE BUTTON')
+     fireEvent.click(getByTestId('outsidePanelButton'))
+     expect(getByTestId('panelButton')).toHaveTextContent('SHOW BUTTON')
+    
+  })
+
+  it('cleans up the event listeners after component is unmounted', () => {
+    const removeEventListener = jest.spyOn(document, 'removeEventListener');
+    const {getByTestId} = render(<TogglePanelButton />);
+    fireEvent.click(getByTestId('togglePanelButton'));
+    expect(removeEventListener).toHaveBeenCalledTimes(1);
+  })
 })
